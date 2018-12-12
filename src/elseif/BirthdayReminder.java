aa97f55a8e6687e61c8f02891311037789740945
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-12-12 13:54:05 -0800

    Week idk 12/12

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -11,21 +11,33 @@ public class BirthdayReminder {
 	public static void main(String[] args) {
 	
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String momsBirthday = "July 6th";
+		String dadsBirthday = "August 26th";
+		String myBirthday = "March 10th";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+	String birthday = JOptionPane.showInputDialog("Would you like to know about your dad's, mom's, or your birthday?");
 		// 3. Print out what the user typed
-		
+		System.out.println(birthday);
 		// 4. if user asked for "mom"
 			//print mom's birthday
+		if(birthday.equalsIgnoreCase("Mom")) {
+			System.out.println(momsBirthday);
+		}
 		// 5. if user asked for "dad"
 			// print dad's birthday
+		else	if(birthday.equalsIgnoreCase("Dad")) {
+			System.out.println(dadsBirthday);
+		}
 		// 6. if user asked for your name
 			// print myBirthday
+		else	if(birthday.equalsIgnoreCase("Mine")) {
+			System.out.println(myBirthday);
+		}
 		//7. otherwise print "Sorry, i don't remember that person's birthday!"
-
+		else {
+			
+			System.out.println("Sorry, i don't remember that person's birthday!");
+		}
 	} 
 }
