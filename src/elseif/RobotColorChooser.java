f8f46accf64fb14ae921118ccab3f4721172f311
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-12-19 13:53:34 -0800

    WEEK BEFORE CHRISTMAS ;-;

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -4,24 +4,50 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
-		//1. Create a new Robot
-		
-		//3. Ask the user what color they would like the robot to draw
-		
-		//5. Use an if/else statement to set the pen color that the user requested
+		// 1. Create a new Robot
+		Robot meme = new Robot();
+		meme.setSpeed(10);
+		for (int i = 0; i < 6; i++) {
 
-        //6. If the user doesn’t enter anything, choose a random color
+			// 3. Ask the user what color they would like the robot to draw
+			String response = JOptionPane.showInputDialog("What color would you like the robot to draw with?");
+			// 5. Use an if/else statement to set the pen color that the user requested
+			if (response.equalsIgnoreCase("red")) {
+				meme.setPenColor(255, 0, 0);
+			} else if (response.equalsIgnoreCase("blue")) {
+				meme.setPenColor(0, 0, 255);
+			} else if (response.equalsIgnoreCase("Teal")) {
+				meme.setPenColor(0, 255, 188);
+			} else if (response.equalsIgnoreCase("Magenta")) {
+				meme.setPenColor(255, 0, 239);
+			} else if (response.equalsIgnoreCase("green")) {
+				meme.setPenColor(77, 255, 0);
+			}
+			// 6. If the user doesn’t enter anything, choose a random color
+			else if (response.equalsIgnoreCase("")) {
+				meme.setRandomPenColor();
+			}
+			// 7. Put a loop around your code so that you keep asking the user for more
+			// colors & drawing them
 
-        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
-		//4. Set the pen width to 10
-		
-	    //2. Make the robot draw a shape (this will take more than one line of code)
-
-
+			// 4. Set the pen width to 10
+			meme.setPenWidth(10);
+			// 2. Make the robot draw a shape (this will take more than one line of code)
+			meme.move(50);
+			meme.turn(90);
+			meme.move(50);
+			meme.turn(90);
+			meme.move(50);
+			meme.turn(90);
+			meme.move(50);
+			meme.turn(90);
+		}
 	}
 }
--- src/extra/BananaQuiz.java
+++ src/extra/BananaQuiz.java
@@ -10,15 +10,22 @@ public class BananaQuiz
 	public static void main(String[] args)
 	{
 		//1. ask the user if they like bananas
+		String answer = JOptionPane.showInputDialog("Do you like the yellow/green products of a specific species of tree that conatins a lot of potassium?");
 		//2. if they say no, 
+		if(answer.equalsIgnoreCase("no")) {
+			JOptionPane.showMessageDialog(null, "You are a spawn of the darkest forces in thi multiverse");
+			System.exit(0);
 			//tell them they are crazy 
 			//and end quiz
+		}
 		//3. if they say yes
+		else if (answer.equalsIgnoreCase("yes")) {
+			
 		//	ask them what is their favorite hobby
 		//	show a pop up that says "<your hobby> is much better with bananas!"
-
-		//4. OPTIONAL: if they say something other than “yes�? or “no�?
-		//	show a pop up that says “You are bananas!�?
+		}
+		//4. OPTIONAL: if they say something other than “yes�? or “no�?
+		//	show a pop up that says “You are bananas!�?
 	
 	}
 
